f6a2c95d7ba6982d8d55fe1c1072e5c7fd163142
Georg Sauthoff <mail@georg.so>
2017-12-16 11:01:31 +0100

    add some more privacy items from my about:config

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -29,6 +29,31 @@ user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.trackingprotection.enabled", true);
 // privacy.trackingprotection.introCount 20
 
+// we don't want to give Mozilla a carte blanche for installing
+// any addon they feel like without further opt-in
+// yes, it's enabled, by default, in Firefox 57
+// case in point:
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1423003
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1424977
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1425171
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1425187
+// https://support.mozilla.org/en-US/questions/1194583
+// https://www.reddit.com/r/firefox/comments/7jh9rv/what_is_looking_glass/
+// http://www.businessinsider.com/firefoxs-mr-robot-marketing-stunt-goes-wrong-users-think-they-downloaded-virus-2017-12
+user_pref("app.shield.optoutstudies.enabled", false);
+
+// do we really trust mozilla enough to phone home for a more and more
+// incomprehensive list of things?
+// master kill switch
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1195552#c4
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+
+// more specific
+// user_pref("datareporting.healthreport.uploadEnabled", false);
+// also disable local collection
+// user_pref("toolkit.telemetry.enabled", false);
+
+
 // legacy
 // security.enable_java false
 // security.warn_viewing_mixed false
